c3d66841c11a40b38438713beb4a8d97fabf21df
mixereqv1 <mateuszral7@gmail.com>
2020-01-05 23:32:42 +0100

    Add basic search and styles

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,43 @@
+<template>
+    <input id="search" name="search" :value="value" @input="handleChange" />
+</template>
+
+<script>
+
+export default {
+  name: 'SearchInput',
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    handleChange(e) {
+      this.$emit('input', e.target.value);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+    input {
+        margin-top: 50px;
+        color: #fff;
+        text-align: center;
+        font-size: 18px;
+        font-weight: 300;
+        height: 30px;
+        border: 0;
+        background: none;
+        border-bottom: 1px solid #fff;
+        transition: box-shadow .5s ease-out;
+        @media (min-width: 768px) {
+          font-weight: 400;
+        }
+        &:focus {
+          outline: none;
+          box-shadow: 0 10px 20px -8px rgba(255,255,255, .5);
+        }
+      }
+</style>
