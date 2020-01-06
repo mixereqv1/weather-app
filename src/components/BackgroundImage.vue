6134824566f36c70fa62316e12f8fb8f65eede63
mixereqv1 <mateuszral7@gmail.com>
2020-01-06 12:58:22 +0100

    Add background image and styles to it

---
+++ src/components/BackgroundImage.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="heroImage"></div>
+</template>
+
+<script>
+
+export default {
+  name: 'BackgroundImage',
+
+};
+</script>
+
+<style lang="scss">
+    .heroImage {
+        position: fixed;
+        top: 0;
+        z-index: -1;
+        width: 100%;
+        height: 100vh;
+        background: url('../assets/backgroundImage.jpg') no-repeat;
+        background-size: cover;
+        background-position: 50% 0%;
+
+        &::after {
+            content: '';
+            position: absolute;
+            background: rgba(0,0,0, .3);
+            top: 0;
+            left: 0;
+            z-index: 1;
+            width: 100%;
+            height: 100vh;
+        }
+    }
+</style>
